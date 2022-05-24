e9dbd23ac480609ce0d99b3f247aff1cdd0d1daa
Ilham Adhi <ilhamfirmanto3@gmail.com>
2022-05-24 21:13:32 +0700

    Submit tugas test script

---
+++ tugas-testscript.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe('My First Test', () => {
+    it('clicking "type" shows the right headings', () => {
+
+        cy.visit('https://example.cypress.io')
+
+        cy.pause()
+
+        cy.contains('type').click()
+        
+        // Should be on a new URL which include 'commands/actions'
+  
+        cy.url().should('include', '/commands/actions')
+
+        // Get an input, type into it and verify that the value has been updated
+        cy.get('.action-email')
+        .type('ilham@email.com').should('have.value', 'ilham@email.com')
+    })
+})
