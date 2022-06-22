9c97ee88da74b838364c19634b34c701dc3858c3
Ilham Adhi <ilhamfirmanto3@gmail.com>
2022-06-22 20:22:55 +0700

    File Step Definitions

---
+++ loginStep.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress"/>
+
+import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
+Given('I open login page', () => {
+    cy.visit('http://zero.webappsecurity.com/login.html')
+})
+
+When('I submit login', () => {
+    cy.get('#user_login').type('username')
+    cy.get('#user_password').type('password')
+    cy.contains('Sign in').click()
+})
+
+Then('I should see homepage', () => {
+    cy.get('#account_summary_tab').should('be.visible')
+})
